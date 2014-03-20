2ed8e41f51d9534d0c3b598ae26c4769c07b7d40
Jim Darden <jdarden@dardenhome.com>
2014-03-19 20:34:45 -0700

    test checkin

--- src/main/java/com/mwambachildrenschoir/act/dao/DonorEntity.java
+++ src/main/java/com/mwambachildrenschoir/act/dao/DonorEntity.java
@@ -21,6 +21,7 @@ public class DonorEntity {
 	@GeneratedValue
 	private Integer id;
 
+	// 
 	@OneToMany(mappedBy="donor", cascade = {CascadeType.ALL})
 	private List<DonationEntity> transactions;
 	
